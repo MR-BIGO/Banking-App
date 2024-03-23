0807f3bc6c2712b163ad14fbd4eca0cd4cd15a29
gabi <sandrogabidauri26@gmail.com>
2024-03-23 18:09:24 +0400

    fixed bottom navigation visibility

--- app/src/main/java/com/example/bankingapp/presentation/screen/main/MainFragment.kt
+++ app/src/main/java/com/example/bankingapp/presentation/screen/main/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.example.bankingapp.presentation.screen.main
 
+import android.view.View
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.bankingapp.R
@@ -16,10 +17,22 @@ class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::infl
         val navController = navHostFragment.navController
         bottomNavigationView.setupWithNavController(navController)
 
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.newCardFragment) {
+                binding.bottomNavigationView.visibility = View.GONE
+            } else if (destination.id == R.id.paymentFragment) {
+                binding.bottomNavigationView.visibility = View.GONE
+            } else if (destination.id == R.id.transferToOwnAccFragment) {
+                binding.bottomNavigationView.visibility = View.GONE
+            } else if (destination.id == R.id.transferToSomeoneElseFragment) {
+                binding.bottomNavigationView.visibility = View.GONE
+            } else if (destination.id == R.id.exchangeFragment) {
+                binding.bottomNavigationView.visibility = View.GONE
+            } else {
+                binding.bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
     }
 
-    override fun observers() {
-
-    }
 
 }
---
+++ app/src/main/java/com/example/bankingapp/presentation/screen/transactions/to_someone_else/TransferToSomeoneElseFragment.kt
@@ -0,0 +1,17 @@
+package com.example.bankingapp.presentation.screen.transactions.to_someone_else
+
+import com.example.bankingapp.databinding.FragmentTransferToSomeoneElseBinding
+import com.example.bankingapp.presentation.base.BaseFragment
+
+class TransferToSomeoneElseFragment :
+    BaseFragment<FragmentTransferToSomeoneElseBinding>(FragmentTransferToSomeoneElseBinding::inflate) {
+
+    override fun setUp() {
+
+    }
+
+    override fun listeners() {
+
+    }
+
+}
