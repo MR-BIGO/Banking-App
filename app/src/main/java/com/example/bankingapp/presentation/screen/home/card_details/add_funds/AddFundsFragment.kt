f0701d8306b741a89179b5c0f84e57228c5b117e
unknown <daviti.okruashvili.1@btu.edu.ge>
2024-03-24 19:11:51 +0400

    changes

---
+++ app/src/main/java/com/example/bankingapp/presentation/screen/home/card_details/add_funds/AddFundsFragment.kt
@@ -0,0 +1,10 @@
+package com.example.bankingapp.presentation.screen.home.card_details.add_funds
+
+import com.example.bankingapp.databinding.FragmentAddFundsBinding
+import com.example.bankingapp.presentation.base.BaseFragment
+
+
+class AddFundsFragment : BaseFragment<FragmentAddFundsBinding>(FragmentAddFundsBinding::inflate) {
+
+
+}
